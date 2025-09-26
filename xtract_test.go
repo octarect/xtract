84db7b9b7e9a2f8580def6b0c1f592d903244f61
Ryota Kota <ryota.kota@member.fsf.org>
2025-09-26 21:36:22 +0900

    refactor: clean up tests

--- xtract_test.go
+++ xtract_test.go
@@ -10,114 +10,59 @@ import (
 	"golang.org/x/net/html"
 )
 
-func TestDecodeError(t *testing.T) {
-	type Result struct{}
+func TestDecode(t *testing.T) {
+	doc := "<span>foo</span"
 
-	// Tests that the function properly returns errors for invalid arguments.
-	tests := []any{
-		Result{}, // Non pointer
-		nil,
+	type result struct {
+		Field string `xpath:"//span"`
 	}
-	for _, tv := range tests {
-		t.Run(fmt.Sprintf("%+v", tv), func(t *testing.T) {
-			r := strings.NewReader("")
-			err := NewDecoder(r).Decode(tv)
-			if err == nil {
-				t.Errorf("%+v should be rejected", tv)
+	rslt := result{}
+
+	tests := []struct {
+		name    string
+		input   any
+		want    any
+		wantErr bool
+	}{
+		{"nil should be rejected", nil, nil, true},
+		{"non-pointer should be rejected", "", "", true},
+		{"success", &rslt, &result{"foo"}, false},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			r := strings.NewReader(doc)
+			err := NewDecoder(r).Decode(tt.input)
+			if tt.wantErr == (err == nil) {
+				t.Errorf("unexpected error status: %v", err)
+			}
+			if !reflect.DeepEqual(tt.input, tt.want) {
+				t.Errorf("unexpected result. got=%+v, expected=%+v", tt.input, tt.want)
 			}
 		})
 	}
 }
 
-func testDecode[T any](t *testing.T, doc string, result, expected T) {
-	r := strings.NewReader(doc)
-	err := NewDecoder(r).Decode(&result)
-	if err != nil {
-		t.Fatal(err)
-	}
-	if !reflect.DeepEqual(result, expected) {
-		t.Errorf("Unexpected result. got=%+v, expected=%+v", result, expected)
+type errReader struct{}
+
+func (r *errReader) Read([]byte) (int, error) {
+	return 0, fmt.Errorf("error")
+}
+
+func TestDecodeInvalidDocument(t *testing.T) {
+	err := NewDecoder(&errReader{}).Decode(new(string))
+	if err == nil {
+		t.Fatal("invalid document should be rejected")
 	}
 }
 
-func TestDecodeString(t *testing.T) {
-	doc := `
-	<html>
-		<body>
-			<span class="string">string</span>
-		</body>
-	</html>`
-
-	type Result struct {
-		String    string `xpath:"//span[@class=\"string\"]"`
-		StringPtr *string `xpath:"//span[@class=\"string\"]"`
-	}
-
-	str := "string"
-	strPtr := &str
-	expected := Result{
-		String: str,
-		StringPtr: strPtr,
-	}
-
-	testDecode(t, doc, Result{}, expected)
-}
-
-func TestDecodeNestedStruct(t *testing.T) {
-	doc := `
-	<html>
-		<body>
-			<span class="string">string</span>
-		</body>
-	</html>`
-
-	type Nested struct {
-		Field string `xpath:"//span[@class=\"string\"]"`
-	}
-	type Result struct {
-		Nested
-	}
-
-	expected := Result{
-		Nested: Nested{
-			Field: "string",
-		},
-	}
-
-	var result Result
-	testDecode(t, doc, result, expected)
-}
-
-type CustomTime struct {
+type customTime struct {
 	time.Time
 }
 
-func (t *CustomTime) UnmarshalXPath(data []byte) (err error) {
+func (t *customTime) UnmarshalXPath(data []byte) (err error) {
 	t.Time, err = time.Parse("2006-01-02 15:04:05", string(data))
 	return
-}
-
-var CustomTimeExample = CustomTime{time.Date(1970, 1, 1, 0, 0, 0, 0, time.UTC)}
-
-func TestCustomUnmarshaler(t *testing.T) {
-	var doc = `
-	<!DOCTYPE html>
-	<html lang="en">
-		<body>
-			<span class="time">1970-01-01 00:00:00</span>
-		</body>
-	</html>`
-
-	type Result struct {
-		Time CustomTime `xpath:"//span[@class=\"time\"]"`
-	}
-
-	expected := Result{
-		Time: CustomTime{time.Date(1970, 1, 1, 0, 0, 0, 0, time.UTC)},
-	}
-
-	var result Result
-	testDecode(t, doc, result, expected)
 }
 
 func TestUnmarshal(t *testing.T) {
@@ -200,54 +145,28 @@ func TestUnmarshal(t *testing.T) {
 }
 
 func TestDereference(t *testing.T) {
-	type TestData struct {
-		Time    CustomTime
-		TimePtr CustomTime
-	}
+	str := "foo"
 
-	str := "concrete"
-	strPtr := &str
-	st := TestData{
-		Time:    CustomTimeExample,
-		TimePtr: CustomTimeExample,
+	type result struct {
+		Field string
+		Time  customTime
+	}
+	tm := customTime{time.Date(1970, 1, 1, 0, 0, 0, 0, time.UTC)}
+	st := result{
+		Field: str,
+		Time:  tm,
 	}
 
 	tests := []struct {
 		name           string
 		input          reflect.Value
-		expected       any
+		want           any
 		hasUnmarshaler bool
 	}{
-		{
-			"string",
-			reflect.ValueOf(str),
-			str,
-			false,
-		},
-		{
-			"string pointer",
-			reflect.ValueOf(strPtr),
-			str,
-			false,
-		},
-		{
-			"string pointer to pointer",
-			reflect.ValueOf(&strPtr),
-			str,
-			false,
-		},
-		{
-			"struct field",
-			reflect.ValueOf(&st).Elem().Field(0),
-			CustomTimeExample,
-			true,
-		},
-		{
-			"struct field pointer",
-			reflect.ValueOf(&st).Elem().Field(1),
-			CustomTimeExample,
-			true,
-		},
+		{"underlying", reflect.ValueOf(str), str, false},
+		{"pointer", reflect.ValueOf(&str), str, false},
+		{"struct field", reflect.ValueOf(&st).Elem().Field(0), str, false},
+		{"unmarshaler", reflect.ValueOf(&st).Elem().Field(1), tm, true},
 	}
 
 	for _, tt := range tests {
@@ -255,15 +174,14 @@ func TestDereference(t *testing.T) {
 			got, u := dereference(tt.input)
 
 			// Compare dereferenced value (actual value) with expected value
-			if got.IsValid() && got.Interface() != tt.expected {
-				t.Errorf("dereference(%T) = %v; want %v", tt.input, got, tt.expected)
+			if got.IsValid() && got.Interface() != tt.want {
+				t.Errorf("dereference(%T) = %v; want %v", tt.input, got, tt.want)
 			}
 
 			hasUnmarshaler := u != nil
 			if hasUnmarshaler != tt.hasUnmarshaler {
 				t.Errorf("dereference(%T) unmarshaler = %v; want %v", tt.input, hasUnmarshaler, tt.hasUnmarshaler)
 			}
-
 		})
 	}
 }
@@ -271,11 +189,11 @@ func TestDereference(t *testing.T) {
 // Unexported fields are not addressable, so Unmarshaler cannot be detected.
 // This is the same limitation as the `encoding/json` package.
 func TestDereferenceUnexportedField(t *testing.T) {
-	type TestData struct {
-		time CustomTime
+	type testData struct {
+		time customTime
 	}
-	data := TestData{
-		time: CustomTimeExample,
+	data := testData{
+		time: customTime{time.Date(1970, 1, 1, 0, 0, 0, 0, time.UTC)},
 	}
 
 	v := reflect.ValueOf(&data).Elem().Field(0)
